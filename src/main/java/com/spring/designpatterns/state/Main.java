8262e359746715e1779d44957de3cd980b38f158
PurpsDope <74745250+PurpsDope@users.noreply.github.com>
2023-04-19 09:53:39 +0300

    Added comment blocks to State Main Class.

--- src/main/java/com/spring/designpatterns/state/Main.java
+++ src/main/java/com/spring/designpatterns/state/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
         MediaPlayer mediaPlayer = new MediaPlayer();
 
-        mediaPlayer.play();
-        mediaPlayer.play();
-        mediaPlayer.stop();
-        mediaPlayer.stop();
+        mediaPlayer.play(); // Output: Started playing... -> Stopped state starts playing.
+        mediaPlayer.play(); // Output: [IGNORED] - Already playing! -> Current state was changed to playing, so it ignores the play again command.
+        mediaPlayer.stop(); // Output: Stopped playing. -> Playing state stops.
+        mediaPlayer.stop(); // Output: [IGNORED] - Already stopped. -> Current state was changed to stopped, so it ignores the stop again command.
     }
 }
